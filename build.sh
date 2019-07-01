fcd2e9d1107f418aa4917974fb87f42630dbd875
Emil Guseynov <theluckyemil@gmail.com>
2019-07-01 17:57:28 +0300

    fix build.sh

--- build.sh
+++ build.sh
@@ -8,6 +8,6 @@ NC='\033[0m'
 CORES=`getconf _NPROCESSORS_ONLN`
 mkdir -p build
 pushd build &> /dev/null
-cmake ../
+cmake ../ -DCMAKE_MODULE_PATH=/opt/eosio/lib/cmake/haya
 make -j${CORES}
 popd &> /dev/null
